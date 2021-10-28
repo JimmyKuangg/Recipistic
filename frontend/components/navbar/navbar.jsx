2d452afff39b5a1090d866c907399615a107fe48
Jimmy Kuang <jimmy.kuang96@gmail.com>
2021-10-28 12:24:37 -0700

    Navbar links open in a new tab

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,72 +1,98 @@
 import React from 'react';
 
 export default class Navbar extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.openFavorites = this.openFavorites.bind(this);
   }
 
   openFavorites() {
     if (!this.props.currentUser) {
-    this.props.openModal('login');
+      this.props.openModal('login');
     } else {
       this.props.history.push('/favorites');
     }
   }
 
-  fetchButtons(){
-    if (!this.props.currentUser){
+  fetchButtons() {
+    if (!this.props.currentUser) {
       return (
         <div className="session-buttons">
-          <button onClick={this.props.openSidemenu}><span className="material-icons">menu</span></button>
-          <button onClick={this.openFavorites}><span className="material-icons">favorite</span></button>
-          <button onClick={() => this.props.openModal('signup')}>Sign Up</button>
+          <button onClick={this.props.openSidemenu}>
+            <span className="material-icons">menu</span>
+          </button>
+          <button onClick={this.openFavorites}>
+            <span className="material-icons">favorite</span>
+          </button>
+          <button onClick={() => this.props.openModal('signup')}>
+            Sign Up
+          </button>
           <button onClick={() => this.props.openModal('login')}>Login</button>
         </div>
-      )
+      );
     } else {
-      return(
+      return (
         <div className="session-buttons">
-          <button onClick={this.props.openSidemenu}><span className="material-icons">menu</span></button>
-          <button onClick={this.openFavorites}><span className="material-icons">favorite</span></button>
+          <button onClick={this.props.openSidemenu}>
+            <span className="material-icons">menu</span>
+          </button>
+          <button onClick={this.openFavorites}>
+            <span className="material-icons">favorite</span>
+          </button>
         </div>
-      )
+      );
     }
   }
 
-  rightNav(){
-    return(
-    <div id="right-nav">
-      <a className="info-link" href="https://github.com/JimmyKuangg/Recipistic">
-        <i className="fab fa-github" />
-      </a>
-      <a className="info-link" href="https://www.linkedin.com/in/jimmy-kuang-789967183/">
-        <i className="fab fa-linkedin"/>
-      </a>
-      {this.props.location.pathname !== "/recipes" ? 
-        <button id="nav-search" onClick={() => this.props.openModal('search')}><i className="fas fa-search"></i></button>
-      : ''}
-    </div>
-    )
+  rightNav() {
+    return (
+      <div id="right-nav">
+        <a
+          className="info-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/JimmyKuangg/Recipistic"
+        >
+          <i className="fab fa-github" />
+        </a>
+        <a
+          className="info-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/jimmy-kuang-789967183/"
+        >
+          <i className="fab fa-linkedin" />
+        </a>
+        {this.props.location.pathname !== '/recipes' ? (
+          <button
+            id="nav-search"
+            onClick={() => this.props.openModal('search')}
+          >
+            <i className="fas fa-search"></i>
+          </button>
+        ) : (
+          ''
+        )}
+      </div>
+    );
   }
 
   render() {
     return (
       <div id="navbar">
         <ul>
+          <li>{this.fetchButtons()}</li>
           <li>
-            {this.fetchButtons()}
-          </li>
-          <li>
-            <button id="logo-button" onClick={() => this.props.history.push('/')}>
+            <button
+              id="logo-button"
+              onClick={() => this.props.history.push('/')}
+            >
               <h1 id="logo">Recipistic</h1>
             </button>
           </li>
-          <li>
-            {this.rightNav()}
-          </li>
+          <li>{this.rightNav()}</li>
         </ul>
       </div>
-    )
+    );
   }
 }
